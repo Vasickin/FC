902dde9cc1b4fba95ae99f03688576074cff8dea
Lex <vasickin@mail.ru>
2024-04-14 18:19:45 +0300

    fill the array

--- Final_control.cs
+++ Final_control.cs
@@ -1,5 +1,13 @@
 ﻿// Задаем размер массива
-        Console.WriteLine("Введите размер массива:");
-        int size = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите размер массива:");
+int size = int.Parse(Console.ReadLine()!);
 
-        
+// Создаем массив строк указанного размера
+string[] array = new string[size];
+
+// Заполняем массив
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите элемент массива {i}:");
+    array[i] = Console.ReadLine()!;
+}
